df371308b94d35ff15eb41b542eddd945c946643
北極狐 <cheung950624@gmail.com>
2023-02-02 19:11:02 +0800

    update

--- cmds/ban.py
+++ cmds/ban.py
@@ -6,18 +6,6 @@ import json
 from nextcord.ext import commands
 
 class ban(Cog_Extension):
-  @commands.Cog.listener()
-  async def on_member_join(self,mem):
-    with open('list.json','r',encoding='utf8') as loli:
-      loli1=json.load(loli)
-      if mem.id in loli1['ban']:
-        await mem.ban(reason='機器人行為')
-        c=self.bot.get_channel(1028977856287604736)
-        await c.send(f"{mem.mention}Got caught still play play")
-      else:
-        c=self.bot.get_channel(1028977856287604736)
-        await c.send(f"{mem.mention} Welcome to 新时代动漫展团")
-
   @commands.command()
   @commands.has_any_role(1028710466664550490,1028710721942454292,"工程師")
   @commands.has_permissions(ban_members=True)
