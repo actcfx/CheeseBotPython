b07d139b9f4048c4d93ce9d159539aec61aa0c42
北極狐 <91001011+actcfx@users.noreply.github.com>
2024-04-21 01:13:20 +0800

    完成指令的 slashcommand 化，並且優化了例外處理

--- cmds/admin/purge.py
+++ cmds/admin/purge.py
@@ -1,18 +1,35 @@
 import nextcord
 from nextcord.ext import commands
-from core.classes import Cog_Extension
+from nextcord import Interaction, SlashOption
+from core.classes import Cog_Extension, ErrorHandler
+
 
 class mod(Cog_Extension):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def purge(self, ctx, limit: int):
-        if ctx.author.guild_permissions.manage_messages:  # Check if the member has the required permission
-            await ctx.channel.purge(limit=limit+1)
-            await ctx.send(f'{limit} messages deleted', delete_after=5)
-        else:
-            await ctx.send("You do not have the required permissions to use this command.", delete_after=5)
+    @nextcord.slash_command(name="purge", description="批量刪除訊息")
+    @commands.has_guild_permissions(manage_messages=True)
+    async def purge(
+        self,
+        interaction: Interaction,
+        limit: int = SlashOption(name="刪除數量", required=True, min_value=1),
+    ):
+        await interaction.response.defer(ephemeral=True)
+
+        try:
+            await interaction.channel.purge(limit=limit)
+            await interaction.followup.send(f"已刪除 {limit} 則訊息")
+
+        except Exception as unexpected_error:
+            await ErrorHandler.handle_error(
+                self,
+                interaction,
+                command="purge",
+                error_content=unexpected_error,
+                error_type="unexpected_error",
+            )
+
 
 def setup(bot):
-        bot.add_cog(mod(bot))
+    bot.add_cog(mod(bot))
