facb40d61e54181cb05ee12e78c2d917e6ad6a6d
北極狐 <91001011+actcfx@users.noreply.github.com>
2024-05-09 10:56:39 +0800

    refactor: 添加無法發送私訊的例外處理

--- cmds/member/leave.py
+++ cmds/member/leave.py
@@ -1,46 +1,29 @@
 import json
 import nextcord
+from nextcord import TextChannel
 from nextcord.ext import commands
-from core.classes import Cog_Extension
+from core.classes import Cog_Extension, ConfigData
 
 
 class Leave(Cog_Extension):
-    DATA_FILE = "data/list.json"
-    CHARLOTTE_LEAVE_CHANNEL_ID = 1167041977133580309
-
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        charlotte_leave_channel = self.bot.get_channel(self.CHARLOTTE_LEAVE_CHANNEL_ID)
-        # banlast = self.load_data()
+        GUILD_ID: int = ConfigData.load_data("config/bot_info.json").get("guild_id")
+        LEAVE_CHANNEL: TextChannel = self.bot.get_channel(ConfigData.load_data("config/channels.json").get("leave_channel"))
+        LEAVE_DM_CONTENT: str = ConfigData.load_data("config/assets.json").get("leave_dm_content")
 
-        # if member.id in banlast["ban"]:
-        #     # Member is in ban list, no further action required.
-        #     pass
-        # elif str(member.id) not in banlast["leavl"]:
-        #     # Member is leaving for the first time.
-        #     banlast["leavl"][str(member.id)] = 1
-        # elif banlast["leavl"][str(member.id)] >= 1:
-        #     # Member has left previously, moving to ban list.
-        #     banlast["ban"].append(member.id)
-        #     del banlast["leavl"][str(member.id)]
-        # else:
-        #     # Increment the leave count for the member.
-        #     banlast["leavl"][str(member.id)] += 1
+        if member.guild.id != GUILD_ID:
+            return
 
-        # self.save_data(banlast)
-        await charlotte_leave_channel.send(f"{member} just left the server")
-        print(f"-> {member} left the server!")
+        await LEAVE_CHANNEL.send(f"{member} just left the server")
 
-    def load_data(self):
-        with open(self.DATA_FILE, "r", encoding="utf-8") as file:
-            return json.load(file)
-
-    def save_data(self, data):
-        with open(self.DATA_FILE, "w", encoding="utf-8") as file:
-            json.dump(data, file, indent=4)
+        try:
+            await member.send(LEAVE_DM_CONTENT)
+        except Exception:
+            print(f"Cannot send dm to {member}")
 
 
 def setup(bot):
