30389715d13cf99eeaec4459e16c558b1d2aa3b3
北極狐 <91001011+actcfx@users.noreply.github.com>
2024-05-10 15:23:44 +0800

    refactor: 調整檔案位置及名稱 guild/reaction_roles.py -> reaction_roles/update.py

--- cmds/reaction_roles/update.py
+++ cmds/reaction_roles/update.py
@@ -1,11 +1,9 @@
-import json
-import nextcord
 from nextcord import Role, Guild
 from nextcord.ext import commands
 from core.classes import Cog_Extension, ConfigData
 
 
-class Role(Cog_Extension):
+class UpdateReactionRoles(Cog_Extension):
     def __init__(self, bot):
         self.bot = bot
 
@@ -15,11 +13,6 @@ class Role(Cog_Extension):
         GUILD: Guild = self.bot.get_guild(GUILD_ID)
 
         REACTION_ROLES_DICT: dict = ConfigData.load_data("data/reaction_roles.json")
-        REACTION_ROLES_GROUPS: list[str] = list(REACTION_ROLES_DICT.keys())
-        REACTION_ROLES_MESSAGES_ID: list[int] = [
-            REACTION_ROLES_DICT.get(roles_group).get("message_id")
-            for roles_group in REACTION_ROLES_GROUPS
-        ]
 
         MEMBER_ROLES_ID_SET: set[int] = {role.id for role in payload.member.roles}
 
@@ -29,18 +22,15 @@ class Role(Cog_Extension):
         target_group_emojis: list[int]
         target_role_index: int
         target_role: Role
-        repeat_emoji_set: set[int]
-        repeat_emoji_list: list[int]
+        repeat_emojis: list[int]
 
         if payload.guild_id != GUILD_ID:
             return
 
-        if payload.message_id not in REACTION_ROLES_MESSAGES_ID:
+        if not REACTION_ROLES_DICT.get(str(payload.message_id)):
             return
 
-        target_group = REACTION_ROLES_DICT.get(
-                    REACTION_ROLES_GROUPS[REACTION_ROLES_MESSAGES_ID.index(payload.message_id)]
-                )
+        target_group = REACTION_ROLES_DICT.get(str(payload.message_id))
         target_group_roles_dict = target_group.get("roles")
         target_group_roles = target_group_roles_dict.get("role_id")
         target_group_emojis = target_group_roles_dict.get("emoji")
@@ -53,14 +43,15 @@ class Role(Cog_Extension):
         if target_group.get("repeatable"):
             return
         else:
-            repeat_emoji_set = MEMBER_ROLES_ID_SET.intersection(target_group_roles)
-            if any(repeat_emoji_set):
-                repeat_emoji_list = list(repeat_emoji_set)
-                for repeat_emoji_id in repeat_emoji_list:
+            repeat_emojis = list(MEMBER_ROLES_ID_SET.intersection(target_group_roles))
+            if any(repeat_emojis):
+                for repeat_emoji_id in repeat_emojis:
                     repeat_emoji_index = target_group_roles.index(repeat_emoji_id)
                     repeat_emoji = target_group_emojis[repeat_emoji_index]
 
-                    message = GUILD.get_channel(payload.channel_id).get_partial_message(payload.message_id)
+                    message = GUILD.get_channel(payload.channel_id).get_partial_message(
+                        payload.message_id
+                    )
                     await message.remove_reaction(repeat_emoji, payload.member)
 
     @commands.Cog.listener()
@@ -69,11 +60,6 @@ class Role(Cog_Extension):
         GUILD: Guild = self.bot.get_guild(GUILD_ID)
 
         REACTION_ROLES_DICT: dict = ConfigData.load_data("data/reaction_roles.json")
-        REACTION_ROLES_GROUPS: list[str] = list(REACTION_ROLES_DICT.keys())
-        REACTION_ROLES_MESSAGES_ID: list[int] = [
-            REACTION_ROLES_DICT.get(roles_group).get("message_id")
-            for roles_group in REACTION_ROLES_GROUPS
-        ]
 
         target_group: dict
         target_group_roles_dict: dict
@@ -85,12 +71,10 @@ class Role(Cog_Extension):
         if payload.guild_id != GUILD_ID:
             return
 
-        if payload.message_id not in REACTION_ROLES_MESSAGES_ID:
+        if not REACTION_ROLES_DICT.get(str(payload.message_id)):
             return
 
-        target_group = REACTION_ROLES_DICT.get(
-            REACTION_ROLES_GROUPS[REACTION_ROLES_MESSAGES_ID.index(payload.message_id)]
-        )
+        target_group = REACTION_ROLES_DICT.get(str(payload.message_id))
         target_group_roles_dict = target_group.get("roles")
         target_group_roles = target_group_roles_dict.get("role_id")
         target_group_emojis = target_group_roles_dict.get("emoji")
@@ -102,4 +86,4 @@ class Role(Cog_Extension):
 
 
 def setup(bot):
-    bot.add_cog(Role(bot))
+    bot.add_cog(UpdateReactionRoles(bot))
