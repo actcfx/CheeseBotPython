ea7e24f841a1339a6ddd45aa1be96d79714d0509
北極狐 <91001011+actcfx@users.noreply.github.com>
2024-06-27 02:52:02 +0800

    feat: 1. 修改討論串名稱格式 2. 增加連結方便進入討論串 1. 將討論串名稱加入討論串類型（ suggest / sponsorship） ，以避免混淆 2. 在建立討論串的回覆加入討論串連結，方便進入

--- cmds/guild/sponsorship.py
+++ cmds/guild/sponsorship.py
@@ -10,11 +10,11 @@ class Sponsorship(Cog_Extension):
     @nextcord.slash_command(name="工商合作與贊助", description="社群工商申請")
     async def 工商合作與贊助(self, interaction: Interaction):
         try:
-            await interaction.response.send_message("你的專屬建議與反饋討論串已開啟")
-            suggest_theard = await interaction.channel.create_thread(
-                name=interaction.user.name, type=nextcord.ChannelType.private_thread
+            sponsorship_theard = await interaction.channel.create_thread(
+                name=f"{interaction.user.name} 的專屬工商合作與贊助串", type=nextcord.ChannelType.private_thread
             )
-            await suggest_theard.send(
+            await interaction.response.send_message(f"你的專屬工商合作與贊助討論串已開啟\n{sponsorship_theard.mention}")
+            await sponsorship_theard.send(
                 f"> <@&978958227016409088><@&978956326359138314><@978680963099942912> 有新的建議與反饋唷！"
             )
 
